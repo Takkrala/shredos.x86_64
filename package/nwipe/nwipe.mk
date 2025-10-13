d32a452636da5e05eb7a865ffa6b8dd4aa530073
Dylan <Takkrala@gmail.com>
2025-10-13 15:46:00 +0200

    Update nwipe version to v0.41 and site details

--- package/nwipe/nwipe.mk
+++ package/nwipe/nwipe.mk
@@ -4,9 +4,11 @@
 #
 ################################################################################
 
-NWIPE_VERSION = v0.38
-NWIPE_SITE = $(call github,martijnvanbrummelen,nwipe,$(NWIPE_VERSION))
+NWIPE_VERSION = v0.41
+NWIPE_SITE = $(call github,Takkrala,nwipe,$(NWIPE_VERSION))
 NWIPE_DEPENDENCIES = ncurses parted dmidecode coreutils
+NWipe_SITE_METHOD = tarball
+NWipe_SUBDIR = nwipe-0.41
 
 define NWIPE_INITSH
 	(cd $(@D) && cp ../../../package/nwipe/002-nwipe-banner-patch.sh $(@D) && ./002-nwipe-banner-patch.sh  && PATH="../../host/bin:${PATH}" ./autogen.sh);
