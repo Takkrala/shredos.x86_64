2d8f99b2dfc20ef84fd87ef98fd58b84b673feee
Dylan <Takkrala@gmail.com>
2026-01-14 10:13:59 +0100

    Update NWIPE_VERSION to v1.04

--- package/nwipe/nwipe.mk
+++ package/nwipe/nwipe.mk
@@ -6,12 +6,12 @@
 
 # Select the Git reference based on the Kconfig choice.
 ifeq ($(BR2_PACKAGE_NWIPE_VERSION_STABLE),y)
-NWIPE_VERSION = v1.03
+NWIPE_VERSION = v1.04
 else ifeq ($(BR2_PACKAGE_NWIPE_VERSION_GIT_REVISION),y)
 NWIPE_VERSION = $(call qstrip,$(BR2_PACKAGE_NWIPE_GIT_REVISION))
 else
 # Fallback – should not happen because the choice enforces exactly one option
-NWIPE_VERSION = v1.03
+NWIPE_VERSION = v1.04
 endif
 
 # Default Git repository URL (never empty).
